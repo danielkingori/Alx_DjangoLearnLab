a3933cd7f7e0af854df01edc4dc7332e0eb00120
danielkingori <kingndiritu@gmail.com>
2024-08-18 23:52:43 +0300

    mod

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("login", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
     path("LogoutView", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
-    path('books/add/', add_book, name='add_book'),
-    path('books/edit/<int:pk>/', edit_book, name='edit_book'),
+    path('books/add/<int:pk>/', add_book/, name='add_book'),
+    path('books/edit/<int:pk>/', edit_book/, name='edit_book'),
     path('books/delete/<int:pk>/', delete_book, name='delete_book'),
 ]
