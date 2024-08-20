cc38f1d872f22a232a15d4bd572e69fc7c51e0fa
danielkingori <kingndiritu@gmail.com>
2024-08-20 21:38:46 +0300

    urls

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path('books', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path("register", register, name="register"),
+    path("register", views.register, name="register"),
     path("login", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
     path("LogoutView", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
     path('books/add/<int:pk>/', add_book, name='add_book'),
@@ -17,5 +17,7 @@ urlpatterns = [
     path('member-view/', member_view, name='member_view'),
     
 ]
+
+
 
 from .views import list_books
