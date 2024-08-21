6ef106558f689cc119874ecf45ab15a5207540eb
danielkingori <kingndiritu@gmail.com>
2024-08-21 09:15:12 +0300

    update

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -5,9 +5,9 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True)
     date_of_birth = models.DateField(null=True, blank=True)
-
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+   
