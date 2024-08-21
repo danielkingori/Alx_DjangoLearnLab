500d0789575562a023f97b316b8fa2d3942eee9c
danielkingori <kingndiritu@gmail.com>
2024-08-21 09:06:36 +0300

    update

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class User(AbstractUser):
+class CustomUser(AbstractUser):
     bio = models.TextField(blank=True)
     date_of_birth = models.DateField(null=True, blank=True)
 
