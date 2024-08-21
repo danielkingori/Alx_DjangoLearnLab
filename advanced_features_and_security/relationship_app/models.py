2cc9c8cd2f70331193f7f2041bd790b2f2194317
danielkingori <kingndiritu@gmail.com>
2024-08-21 08:40:15 +0300

    updates

--- advanced_features_and_security/relationship_app/models.py
+++ advanced_features_and_security/relationship_app/models.py
@@ -1,10 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+# from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.conf import settings
+
 
 # Create your models here.
 
+class User(AbstractUser):
+    bio = models.TextField(blank=True)
+    # ... additional fields as needed ...
 class Author(models.Model):
     name = models.CharField(max_length=255)
     def __str__(self):
@@ -42,18 +48,18 @@ class UserProfile(models.Model):
         ('Member', 'Member'),
     ]
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
-    library = models.ForeignKey(Library, on_delete=models.SET_NULL, null=True, blank=True)
-
+    library = models.ForeignKey('Library', on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return f'{self.user.username} - {self.role}'
 
 # Automatically create or update a UserProfile when a User is created or updated
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
     instance.userprofile.save()
     
+
