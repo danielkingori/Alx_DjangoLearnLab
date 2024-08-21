c00097f8cf52a591b3004636875733a9219179f3
danielkingori <kingndiritu@gmail.com>
2024-08-21 08:44:46 +0300

    updates

--- advanced_features_and_security/bookshelf/models.py
+++ advanced_features_and_security/bookshelf/models.py
@@ -1,6 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class User(AbstractUser):
+    bio = models.TextField(blank=True)
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
