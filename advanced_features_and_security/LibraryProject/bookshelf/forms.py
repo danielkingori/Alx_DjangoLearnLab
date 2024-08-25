4ab21d99dc793519cceb9b63fe858650658f88e2
danielkingori <kingndiritu@gmail.com>
2024-08-25 22:21:11 +0300

    advanced security features

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
+from .models import CustomUser, Book
+
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
@@ -11,3 +12,8 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'date_of_birth', 'profile_photo')
+
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author']
