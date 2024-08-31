8e41b118866d419c6f3559e1954c8bc0a20f024f
danielkingori <kingndiritu@gmail.com>
2024-08-31 20:54:19 +0300

    view

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Book
 from rest_framework import viewsets
 from .serializers import BookSerializer
-from rest_framework.generics import ListAPIView
+from rest_framework.generic.ListAPIViews import ListAPIView
 
 # Create your views here.
 
