4b774ee91b7e46c3e8ccdc2ae28c2a466de3c511
danielkingori <kingndiritu@gmail.com>
2024-09-08 22:24:12 +0300

    advanced security features

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from .models import Book
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'book_list.html'
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'book_detail.html'
+
+class BookCreateView(CreateView):
+    model = Book
+    fields = ['title', 'author', 'publication_date']
+    template_name = 'book_form.html'
+
+class BookUpdateView(UpdateView):
+    model = Book
+    fields = ['title', 'author', 'publication_date']
+    template_name = 'book_form.html'
+
+class BookDeleteView(DeleteView):
+    model = Book
+    success_url = '/books/' # redirect after deletion
+    template_name = 'book_confirm_delete.html'
