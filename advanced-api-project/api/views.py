9365653701e150ffa8b02327bffeb39acfefd38d
danielkingori <kingndiritu@gmail.com>
2024-09-08 22:42:17 +0300

    advanced security features

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
 # Create your views here.
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .models import Book
+
+
 
 class BookListView(ListView):
     model = Book
@@ -17,11 +18,23 @@ class BookCreateView(CreateView):
     model = Book
     fields = ['title', 'author', 'publication_date']
     template_name = 'book_form.html'
+    
+    # Custom method to handle form submission
+
+    def form_valid(self, form):
+        if self.request.user.is_authenticated:
+            form.instance.author = self.request.user
+        return super().form_valid(form)
 
 class BookUpdateView(UpdateView):
     model = Book
     fields = ['title', 'author', 'publication_date']
     template_name = 'book_form.html'
+    
+    # Additional logic for filtering or customizing update behavior can go here
+    def form_valid(self, form):
+        form.instance.updated_by = self.request.user
+        return super().form_valid(form)
 
 class BookDeleteView(DeleteView):
     model = Book
