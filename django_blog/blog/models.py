8c981cc98e85940a4d9f24cb60faee65b0fe9266
danielkingori <kingndiritu@gmail.com>
2024-09-22 12:24:38 +0300

    add

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -16,8 +16,8 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE )
-    author =
-    content = 
-    created_at = 
-    updated_at = 
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
     
